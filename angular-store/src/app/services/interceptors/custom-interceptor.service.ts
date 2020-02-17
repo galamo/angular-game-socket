b7961284732212594742e8054a7e38386aef164d
edanofek <edanofek@gmail.com>
2020-02-17 19:20:54 +0200

    interceptor

--- angular-store/src/app/services/interceptors/custom-interceptor.service.ts
+++ angular-store/src/app/services/interceptors/custom-interceptor.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators"
 
 @Injectable()
 export class CustomInterceptorService implements HttpInterceptor {
 
     constructor() { }
-    intercept(request: HttpRequest<any>, next: HttpHandler) {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         //validate token..
         const token = localStorage.getItem("token");
-        if (typeof token !== 'string') return null;
+        if (typeof token !== 'string') return;
 
-        const newRequest = request.clone();
-        newRequest.headers.set("authorization", "token123");
-        console.log("interceptor is working...", newRequest)
+        const newRequest = request.clone({
+            // setHeaders: {
+            //     "authorization": token
+            // }
+        });
+
         console.log(newRequest.headers.get("authorization"))
-        return next.handle(newRequest);
+        return next.handle(newRequest).pipe(map(event => {
+            if (event instanceof HttpResponse) {
+                console.log("inside response interceptor...")
+            }
+            return event;
+        }))
     }
 }
